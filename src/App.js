db7a75cfcd2f3b2672871a9765fa4b9f0c54aa12
Christian Broberg <cb@webhouse.dk>
2017-07-03 23:29:04 +0200

    First code push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+import { List } from 'react-virtualized'
+
+// List data as an array of strings
+const list = [
+    'Brian Vaughn',
+    'Christian Broberg',
+    'Mette Bugge',
+    'Mathilde Porsmose',
+    'Max Porsmose',
+    'Amalie Hayes'
+    
+  // And so on...
+]
+
+function rowRenderer ({
+    key,         // Unique key within array of rows
+    index,       // Index of row within collection
+    isScrolling, // The List is currently being scrolled
+    isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+    style        // Style object to be applied to row (to position it)
+    }) {
+    return (
+        <div key={key} style={style}>
+            {list[index]}
+        </div>
+    )
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
-      </div>
-    )
-  }
+    render() {
+        return (
+        <div>
+            <List
+                width={300}
+                height={300}
+                rowCount={list.length}
+                rowHeight={20}
+                rowRenderer={rowRenderer}
+            />
+        </div>
+        )
+    }
 }
 
 export default App
